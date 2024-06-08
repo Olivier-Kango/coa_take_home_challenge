299da2d24e09a6339ea72e4ba194567742b646d7
Olivier-Kango <olivierkango@gmail.com>
2024-06-08 13:44:16 +0300

    Add next, prev and close buttons

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
-const images = document.querySelectorAll('img');
+const images = document.querySelectorAll('.img-gallery img');
 const wrapper = document.getElementById('wrapper');
 const imgWrapper = document.getElementById('fullImg');
-const close = document.querySelector('span');
+const close = document.querySelector('.close');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
 
-function openModal(pic) {
+let currentIndex = 0;
+
+function openModal(index) {
+  currentIndex = index;
   wrapper.style.display = 'flex';
-  imgWrapper.src = pic;
+  imgWrapper.src = images[currentIndex].src;
 }
 
 images.forEach((img, index) => {
   img.addEventListener('click', () => {
-    openModal(`images/image${index}.png`);
+    openModal(index);
   });
 });
 
 close.addEventListener('click', () => {
   wrapper.style.display = 'none';
+});
+
+prev.addEventListener('click', () => {
+  currentIndex = (currentIndex > 0) ? currentIndex - 1 : images.length - 1;
+  imgWrapper.src = images[currentIndex].src;
+});
+
+next.addEventListener('click', () => {
+  currentIndex = (currentIndex < images.length - 1) ? currentIndex + 1 : 0;
+  imgWrapper.src = images[currentIndex].src;
 });
