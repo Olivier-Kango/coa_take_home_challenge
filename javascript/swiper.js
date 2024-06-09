e4841dbd9f421b5fb6cd5a23d54e49c28ff79e0e
Olivier-Kango <olivierkango@gmail.com>
2024-06-09 04:17:33 +0300

    Add top of 50px

---
+++ javascript/swiper.js
@@ -0,0 +1,53 @@
+// swipe.js
+document.addEventListener('DOMContentLoaded', () => {
+  function showNextImage() {
+    const currentImage = document.querySelector('.image-container.active');
+    const nextImage = currentImage.nextElementSibling;
+    if (nextImage) {
+      currentImage.classList.remove('active');
+      nextImage.classList.add('active');
+    }
+  }
+
+  function showPrevImage() {
+    const currentImage = document.querySelector('.image-container.active');
+    const prevImage = currentImage.previousElementSibling;
+    if (prevImage) {
+      currentImage.classList.remove('active');
+      prevImage.classList.add('active');
+    }
+  }
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const imgGallery = document.querySelector('.img-gallery');
+    let startX = 0;
+
+    imgGallery.addEventListener('touchstart', (e) => {
+      startX = e.touches[0].clientX;
+    });
+
+    imgGallery.addEventListener('touchmove', (e) => {
+      const endX = e.touches[0].clientX;
+      const diffX = startX - endX;
+      if (diffX > 50) {
+        showNextImage();
+      } else if (diffX < -50) {
+        showPrevImage();
+      }
+    });
+
+    imgGallery.addEventListener('mousedown', (e) => {
+      startX = e.clientX;
+    });
+
+    imgGallery.addEventListener('mousemove', (e) => {
+      const endX = e.clientX;
+      const diffX = startX - endX;
+      if (diffX > 50) {
+        showNextImage();
+      } else if (diffX < -50) {
+        showPrevImage();
+      }
+    });
+  });
+});
